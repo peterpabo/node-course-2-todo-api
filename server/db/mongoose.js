03db98bba77a27d15077aec92850ba55c17656a5
peterpaboservice <peterpaboservice@gmail.com>
2018-07-03 15:20:58 +0200

    TEST connection MONGODB

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://peterpaboservice:testen123@ds125241.mlab.com:25241/udemytodoapi');// || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI);//mongoose.connect('mongodb://peterpaboservice:testen123@ds125241.mlab.com:25241/udemytodoapi');// || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
