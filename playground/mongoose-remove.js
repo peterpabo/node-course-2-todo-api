0d535b65852f7d8358cfeab3227102ed82bab691
peterpaboservice <peterpaboservice@gmail.com>
2018-07-03 18:34:48 +0200

    TEST connection mlab

--- playground/mongoose-remove.js
+++ playground/mongoose-remove.js
@@ -5,16 +5,5 @@ const {Todo} = require('./../server/models/todo');
 const {User} = require('./../server/models/user');
 
 Todo.remove({}).then((result) => {
-  console.log(result);
-});
-
-// Todo.findOneAndRemove
-// Todo.findByIdAndRemove
-
-// Todo.findOneAndRemove({_id: '57c4610dbb35fcbf6fda1154'}).then((todo) => {
-//
-// });
-
-Todo.findByIdAndRemove('57c4610dbb35fcbf6fda1154').then((todo) => {
-  console.log(todo);
+    console.log(result);
 });
--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-console.log(process.env.MONGODB_URI);
-mongoose.connect(process.env.MONGODB_URI);
-//mongoose.connect('mongodb://mlab:mlab123@ds125241.mlab.com:25241/udemytodoapi?authMechanism=SCRAM-SHA-1');// || 'mongodb://localhost:27017/TodoApp');
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
