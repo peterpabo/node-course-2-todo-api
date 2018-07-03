5f247609b1f326a43831e9c139fc9a7cdcb3efef
peterpaboservice <peterpaboservice@gmail.com>
2018-07-03 17:13:48 +0200

    TEST connection mlab

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
+console.log(process.env.MONGODB_URI);
 mongoose.connect(process.env.MONGODB_URI);
-// mongoose.connect('mongodb://mlab:mlab123@ds125241.mlab.com:25241/udemytodoapi');// || 'mongodb://localhost:27017/TodoApp');
+//mongoose.connect('mongodb://mlab:mlab123@ds125241.mlab.com:25241/udemytodoapi?authMechanism=SCRAM-SHA-1');// || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
