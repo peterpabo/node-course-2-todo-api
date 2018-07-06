74464cac653817b2cbec14ef2b9d352edc12874b
peterpaboservice <peterpaboservice@gmail.com>
2018-07-06 08:19:32 +0200

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var {ObjectID} = require('mongodb');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -78,6 +79,32 @@ app.delete('/todos/:id', (req, res) => {
     });
 
 });
+
+app.patch('/todos/:id', (req, res) => {         //patch => to update a resource
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);
+
+    if (!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();            //returns number of ms from 1/1/1970 (linux), neg. is from the past
+    } else {
+        body.completed = false;
+        body.completedAt = null;    //database clear field
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+        if (!todo){
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
+    }); //param2 mongo operators
+});
+
 
 // app.listen(3000, () => {
 //     console.log('Started on port 3000');
