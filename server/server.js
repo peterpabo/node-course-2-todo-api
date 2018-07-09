8fafbe14bb7f69fbafd27082445bd6491220f70e
peterpaboservice <peterpaboservice@gmail.com>
2018-07-09 16:33:44 +0200

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -132,6 +132,24 @@ app.get('/users/me', authenticate,  (req, res) => {
 });                //model NOT instance
 
 
+
+app.post('/users/login', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+
+
+    User.findByCredentials(body.email, body.password).then((user) => {
+        return user.generateAuthToken().then((token) => {
+            res.header('x-auth', token).send(user);
+        });        
+    }).catch( (e) => {
+       res.status(400).send();
+    });            
+
+    
+});
+
+
+
 // app.listen(3000, () => {
 //     console.log('Started on port 3000');
 // });
