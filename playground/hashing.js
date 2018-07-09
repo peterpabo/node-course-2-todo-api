593017d7e096b364de32a49ca5cd832b7b3538e6
peterpaboservice <peterpaboservice@gmail.com>
2018-07-09 15:12:00 +0200

    Add tests for GET /users/me and POST /users

--- playground/hashing.js
+++ playground/hashing.js
@@ -3,19 +3,22 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 
 var password ='123abc!';
-/*
+
+
+
 bcrypt.genSalt(10, (err, salt) => {
     bcrypt.hash(password, salt, (err, hash) => {
         console.log(hash);
     });
 });
-*/
 
 var hashedPassword = '$2a$10$xxNw0UrrnPVYtdgSaM.35eEFFjbWmD9tqpNSWXj0idtkbw.Na90LK';
 
 bcrypt.compare(password, hashedPassword, (err, res) => {
     console.log(res);
 });
+
+
 
 /*
 var data = {
